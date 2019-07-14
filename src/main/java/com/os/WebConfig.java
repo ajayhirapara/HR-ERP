4cc6b4ac6f1147a91b64aa07c5cef3c4c56c9a3a
Ajay@123! <hirapara.ajay0@gmail.com>
2019-07-14 20:46:56 +0530

    start work on TheamLeaf

--- src/main/java/com/os/WebConfig.java
+++ src/main/java/com/os/WebConfig.java
@@ -5,10 +5,19 @@ import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
 import javax.sql.DataSource;
 
 @Configuration
-public class WebConfig {
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer{
 	
 	@Autowired
 	DataSource dbSource;
@@ -20,4 +29,21 @@ public class WebConfig {
 	    flyway.setDataSource(dbSource);
 	    return flyway;
 	}
+	
+	@Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/")
+            .setViewName("index");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    }
+	
+	@Bean
+    public ITemplateResolver templateResolver() {
+        ClassLoaderTemplateResolver resolver = new ClassLoaderTemplateResolver();
+        resolver.setPrefix("templates/books/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode(TemplateMode.HTML);
+        resolver.setCharacterEncoding("UTF-8");
+        return resolver;
+    }
 }
